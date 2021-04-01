80013ce58931dc30a44927707d722b9d4f3b297e
afroRoboticist <afroroboticist@gmail.com>
2021-04-01 09:20:47 -0400

    spotify.py updated, models.py updated

--- Spotify_Song_Suggester/app.py
+++ Spotify_Song_Suggester/app.py
@@ -3,8 +3,8 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from .models import DB, Track
-from .spotify import get_info_and_add
+from .models import DB, Track, Song_Cluster, Song_NN, Output
+from .spotify import get_info_and_add, get_features_nn, get_predictions_nn
 
 def create_app():
     app = Flask(__name__)
@@ -20,32 +20,25 @@ def create_app():
         tracks = Track.query.all()
         return render_template("base.html", tracks=tracks)
     
-    
+    @app.route('/reset')
+    def reset():
+        DB.drop_all()
+        DB.create_all()
+        return render_template("base.html", title="RESET", tracks=Track.query.all())
+
+
     @app.route("/user_submitted", methods=["GET", "POST"])
     def user_submitted():
 
         track_name = request.form
-        print(track_name, type(track_name))
-        print(track_name['track_name'])
-        DB_track = get_info_and_add(track_name['track_name'])
-        print('Contents of DB_track',DB_track)
-        track_names = Track.query.all()
+        #print(track_name, type(track_name))
+        #print(track_name['track_name'])
+        model_input = get_features_nn(track_name['track_name'])
+        #DB_track = get_info_and_add(track_name['track_name'])
+        #print('Contents of DB_track',DB_track)
+        track_names = get_predictions_nn(model_input)
+        print(track_names)
 
         return render_template("user.html", track_name=track_name['track_name'], tracks=track_names)
     
     return app
-
-
-
-
-# import uvicorn
-# from fastapi import FastAPI
-
-# app = FastAPI()
-
-# @app.get('/')
-# def home():
-#     return {'hello': 'world'}
-
-# if __name__ == '__main__':
-#     uvicorn.run(app)

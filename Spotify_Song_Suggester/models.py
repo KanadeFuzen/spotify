bd4be241bf80cb791eb4afdb7a53e4b5a8ddf841
afroRoboticist <afroroboticist@gmail.com>
2021-04-01 09:20:47 -0400

    spotify.py updated, models.py updated

--- Spotify_Song_Suggester/models.py
+++ Spotify_Song_Suggester/models.py
@@ -11,3 +11,48 @@ class Track(DB.Model):
 
     def __repr__(self):
         return "<Track {}>".format(self.track)
+
+class Song_Cluster:
+  def __init__(self, acousticness, danceability, duration_ms, energy, 
+               explicit, instrumentalness, key, liveness, loudness, mode,
+               popularity, speechiness, tempo, valence):
+                  self.acousticness = acousticness
+                  self.danceability = danceability
+                  self.duration_ms = duration_ms
+                  self.energy = energy
+                  self.explicit = explicit
+                  self.instrumentalness = instrumentalness
+                  self.key = key
+                  self.liveness = liveness
+                  self.loudness = loudness
+                  self.mode = mode
+                  self.popularity = popularity
+                  self.speechiness = speechiness
+                  self.tempo = tempo
+                  self.valence = valence
+
+class Song_NN:
+  def __init__(self, acousticness, danceability, disc_number, duration_ms, energy, 
+               explicit, instrumentalness, key, liveness, loudness, mode, 
+               speechiness, tempo, time_signature, track_number, valence):
+                  self.acousticness = acousticness
+                  self.danceability = danceability
+                  self.disc_number = disc_number
+                  self.duration_ms = duration_ms
+                  self.energy = energy
+                  self.explicit = explicit
+                  self.instrumentalness = instrumentalness
+                  self.key = key
+                  self.liveness = liveness
+                  self.loudness = loudness
+                  self.mode = mode
+                  self.speechiness = speechiness
+                  self.tempo = tempo
+                  self.time_signature = time_signature
+                  self.track_number = track_number
+                  self.valence = valence
+
+class Output:
+  def __init__(self, name, artist):
+    self.name = name
+    self.artist = artist
--- Spotify_Song_Suggester/spotify.py
+++ Spotify_Song_Suggester/spotify.py
@@ -3,7 +3,10 @@ from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from .models import DB, Track
+import pandas as pd
+from .models import DB, Track, Song_Cluster, Song_NN, Output
+import joblib
+from sklearn.neighbors import NearestNeighbors
 
 sp = spotipy.Spotify(
     client_credentials_manager=SpotifyClientCredentials(
@@ -12,8 +15,7 @@ sp = spotipy.Spotify(
 
 def get_info_and_add(track_name):
     try:
-        track_name='Dance with father'
-        results = sp.search(q='Dance with father', type="track", limit=20)
+        results = sp.search(q='"'+track_name+'"', type="track", limit=20)
 
         for count, DB_track in enumerate(results['tracks']['items']):
             track_id = results['tracks']['items'][count]['id']
@@ -31,39 +33,115 @@ def get_info_and_add(track_name):
             DB.session.commit()
     
         return DB_track
-
     except Exception as e:
         print("Error Processing %s: %s" % (track_name, e))
 
+def get_features_cluster(track_name):
+    user_input = '"'+track_name+'"'
+    search_results = sp.search(q=user_input, type="track", limit=1)
+    search_id = search_results['tracks']['items'][0]['id']
+    search_features = sp.audio_features(tracks=search_id)
 
+    for_dict_cluster = Song_Cluster(acousticness=(search_features[0]['acousticness']),
+                    danceability=(search_features[0]['danceability']),
+                    duration_ms=(search_features[0]['duration_ms']), 
+                    energy=(search_features[0]['energy']),
+                    explicit=(search_results['tracks']['items'][0]['explicit']), 
+                    instrumentalness=(search_features[0]['instrumentalness']),
+                    key=(search_features[0]['key']), 
+                    liveness=(search_features[0]['liveness']),
+                    loudness=(search_features[0]['loudness']), 
+                    mode=(search_features[0]['mode']),
+                    popularity=(search_results['tracks']['items'][0]['popularity']), 
+                    speechiness=(search_features[0]['speechiness']),
+                    tempo=(search_features[0]['tempo']), 
+                    valence=(search_features[0]['valence']))
 
-# track_id_list = []
-#         tracks_list = []
-#         artists_list = []
-#         albums_list = []
+    dictionary_cluster = {
+                  'acousticness': [for_dict_cluster.acousticness],
+                  'danceability': [for_dict_cluster.danceability], 
+                  'duration_ms': [for_dict_cluster.duration_ms],
+                  'energy': [for_dict_cluster.energy], 
+                  'explicit': [for_dict_cluster.explicit],
+                  'instrumentalness': [for_dict_cluster.instrumentalness], 
+                  'key': [for_dict_cluster.key],
+                  'liveness': [for_dict_cluster.liveness],
+                  'loudness': [for_dict_cluster.loudness],
+                  'mode': [for_dict_cluster.mode],
+                  'popularity': [for_dict_cluster.popularity],
+                  'speechiness': [for_dict_cluster.speechiness],
+                  'tempo': [for_dict_cluster.tempo],
+                  'valence': [for_dict_cluster.valence]}
 
-#         for count, track_id in enumerate(results['tracks']['items']):
-#             track_id = results['tracks']['items'][count]['id']
-#             track_id_list.append(track_id)
+    cluster_model_input_df = pd.DataFrame.from_dict(data=dictionary_cluster, orient='columns')
 
-#         for count, track in enumerate(results['tracks']['items']):
-#             track = results['tracks']['items'][count]['name']
-#             tracks_list.append(track)
-        
-#         for count, artist in enumerate(results['tracks']['items']):
-#             artist = results['tracks']['items'][count]['artists'][0]['name']
-#             artists_list.append(artist)
-           
-#         for count, album in enumerate(results['tracks']['items']):
-#             album = results['tracks']['items'][count]['album']['name']
-#             albums_list.append(album)
-        
-#         DB_track = Track(
-#             track_id=track_id_list,
-#             track_name=tracks_list,
-#             artist=artists_list,
-#             album=albums_list
-#             )
-        
-#         DB.session.add(DB_track)
-#         DB.session.commit()
+    return cluster_model_input_df
+
+def get_features_nn(track_name):
+    search_results = sp.search(q=track_name, type="track", limit=1)
+    search_id = search_results['tracks']['items'][0]['id']
+    search_features = sp.audio_features(tracks=search_id)
+
+    for_dict_nn = Song_NN(acousticness=(search_features[0]['acousticness']),
+                   danceability=(search_features[0]['danceability']),
+                   disc_number=(search_results['tracks']['items'][0]['disc_number']), 
+                   duration_ms=(search_features[0]['duration_ms']), 
+                   energy=(search_features[0]['energy']), 
+                   explicit=(search_results['tracks']['items'][0]['explicit']),
+                   instrumentalness=(search_features[0]['instrumentalness']),
+                   key=(search_features[0]['key']),
+                   liveness=(search_features[0]['liveness']),
+                   loudness=(search_features[0]['loudness']),
+                   mode=(search_features[0]['mode']),
+                   speechiness=(search_features[0]['speechiness']),
+                   tempo=(search_features[0]['tempo']),
+                   time_signature=(search_features[0]['time_signature']),
+                   track_number=(search_results['tracks']['items'][0]['track_number']),
+                   valence=(search_features[0]['valence']))
+
+    dictionary_nn = {
+              'acousticness': [for_dict_nn.acousticness],
+              'danceability': [for_dict_nn.danceability],
+              'disc_number': [for_dict_nn.disc_number],
+              'duration_ms': [for_dict_nn.duration_ms],
+              'energy': [for_dict_nn.energy],
+              'explicit': [for_dict_nn.explicit],
+              'instrumentalness': [for_dict_nn.instrumentalness],
+              'key': [for_dict_nn.key],
+              'liveness': [for_dict_nn.liveness],
+              'loudness': [for_dict_nn.loudness],
+              'mode': [for_dict_nn.mode],
+              'speechiness': [for_dict_nn.speechiness],
+              'tempo': [for_dict_nn.tempo],
+              'time_signature': [for_dict_nn.time_signature],
+              'track_number': [for_dict_nn.track_number],
+              'valence': [for_dict_nn.valence]}
+
+    nn_model_input_df = pd.DataFrame.from_dict(data=dictionary_nn, orient='columns')
+
+    return nn_model_input_df
+
+def get_predictions_nn(model_input):
+    spotify = pd.read_csv('data/spotify.csv')
+    spotify['artists'] = spotify['artists'].str.replace("'", "")
+    spotify['artists'] = spotify['artists'].str.replace("[", "")
+    spotify['artists'] = spotify['artists'].str.replace("]", "")
+    nn_model = joblib.load('data/nearest_neighbors.sav')
+    neigh_dist, neigh_ind = nn_model.kneighbors(model_input)
+
+    most_similar = []
+
+    for i in range(len(neigh_ind[0])):
+        song = spotify.iloc[neigh_ind[0][i]]['name']
+        artist = spotify.iloc[neigh_ind[0][i]]['artists']
+        entry = Output(song, artist)
+        most_similar.append(entry)
+
+    most_similar = most_similar[1:]
+    tracks = []
+
+    for i in range(len(most_similar)):
+        suggestion = most_similar[i].name+' by '+most_similar[i].artist
+        tracks.append(suggestion)
+
+    return tracks
